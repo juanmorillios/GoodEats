0675259d379a0783efd723f71b9ba06db413d525
juanmorillios <juanmorillios@gmail.com>
2018-09-28 19:15:10 +0200

    Add RecipeDetailVC

---
+++ GoodEats/Controller/RecipeDetailVC.swift
@@ -0,0 +1,30 @@
+//
+//  RecipeDetailVC.swift
+//  GoodEats
+//
+//  Created by Jmorillo on 28/9/18.
+//  Copyright © 2018 MorilloApps. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
